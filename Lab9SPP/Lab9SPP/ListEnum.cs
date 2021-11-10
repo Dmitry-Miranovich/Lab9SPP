c1a40d03378e303c40432c49e64aa71df3539da3
Dmitry_Miranovich <miranovich.2001@yandex.ru>
2021-11-10 09:35:52 +0300

    Lab 9 ready

---
+++ Lab9SPP/Lab9SPP/ListEnum.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9SPP
+{
+    public class ListEnum<T> : IEnumerator<T>
+    {
+
+        private DynamicList<T> list;
+        private int position = -1;
+        public ListEnum(DynamicList<T> list)
+        {
+            this.list = list;
+        }
+
+        public T Current
+        {
+            get
+            {
+                try
+                {
+                    return (T)list[position];
+                }
+                catch(IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < list.Count);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
---
+++ Lab9SPP/Lab9SPP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Lab9SPP
+{
+    class Program
+    {
+        static void Main()
+        {
+            DynamicList<string> list = new DynamicList<string>();
+            list.Add("Hi");
+            list.Add("Pleasure to meet you!");
+            list.Add("Im tom");
+            
+            foreach(string s in list)
+            {
+                WriteLine(s);
+            }
+        }
+    }
+}
